4c4dbfc4362fa938d1648776ed6933fe7c68425d
angeljpr6 <angelpr.dam@gmail.com>
2023-03-31 19:02:40 +0100

    version 1

---
+++ dist/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"postnet","l":"EntidadFinanciera"},{"p":"postnet","l":"Posnet"},{"p":"postnet","l":"Principal"}];updateSearchResults();
---
+++ src/postnet/Principal.java
@@ -0,0 +1,31 @@
+
+package postnet;
+/**
+ * Implementacion de un datáfono sencillo con pago a plazos.
+ * Ejemplo integrador clase Principal
+ * @author 1dam
+ */
+public class Principal {
+    /**
+     * Este es el metodo principal
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Posnet posnet=new Posnet();
+        Persona titular=new Persona("45340173C","Paco","Paquirri","928756478","pepitoPaquirri@gmail.com");
+        TarjetaDeCredito tarjeta=new TarjetaDeCredito("Facebank","41547898745",15000,titular,EntidadFinanciera.BIRZA);
+        
+        System.out.print("Tarjeta antes del pago: ");
+        System.out.print(tarjeta+"\n");
+                
+        System.out.print("Ticket tras pago: ");
+        Ticket ticketGenerado=posnet.efectuarPago(tarjeta, 10000, 5);
+        System.out.print(ticketGenerado +"\n");
+        
+        System.out.print("Tarjeta depues del pago: ");
+        System.out.print(tarjeta+"\n");                      
+    }
+    
+}
